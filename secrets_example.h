94a80d55916241b978462b9704930a78f7cfe0e1
Lee <caroleandlee@gmail.com>
2024-06-05 09:57:50 +1000

    Add exmaple secrets file

---
+++ secrets_example.h
@@ -0,0 +1,17 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+const char* WIFI_SSID = "yourSSID";
+const char* WIFI_PASSWORD = "yourWiFiPassword";
+
+// MQTT credentials
+const char* MQTT_BROKER = "yourMqttBrokerAddress";
+static signed int MQTT_PORT = 1883;
+const char* MQTT_USERNAME = "yourMqttUsername";
+const char* MQTT_PASSWORD = "yourMqttPassword";
+
+// Any other sensitive information
+
+#endif
+
