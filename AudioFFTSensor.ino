1ae664d6f93b62f90cf59b125e871ec76dafaabb
Lee <caroleandlee@gmail.com>
2024-06-06 12:15:59 +1000

    Working with Washing Machine and MQTT to HA

--- AudioFFTSensor.ino
+++ AudioFFTSensor.ino
@@ -4,6 +4,10 @@
 #include <WiFi.h>
 #include <ArduinoMqttClient.h>
 #include "secrets.h"
+//#include "mqtt_discovery.h"
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <WebServer.h>  //Used for HTTP callback to enable/disable discovery
 
 //#define MQTT_BROKER     "homeassistant.local"
 //#define MQTT_PORT       1883
@@ -39,8 +43,23 @@ static unsigned int mx = 0;
 static unsigned int cnt = 0;
 static unsigned long lastTrigger[2] = {0, 0};
 
+//Auto-discover enable/disable option
+bool auto_discovery = false;      //default to false and provide end-user interface to allow toggling 
+
+//Variables for creating unique entity IDs and topics
+byte macAddr[6];                 //Device MAC address
+char uidPrefix[] = "Audio";      //Prefix for unique ID generation (limit to 20 chars)
+char devUniqueID[30];             //Generated Unique ID for this device (uidPrefix + last 6 MAC characters) 
+
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
+WebServer server(80);
+
+
+// Handle root URL
+void handleRoot() {
+  server.send(200, "text/html", "<h1>Welcome to the ESP32 Web Server!</h1>");
+}
 
 static void integerToFloat(int32_t *integer, float *vReal, float *vImag, uint16_t samples)
 {
@@ -191,6 +210,35 @@ void setup(void)
     Serial.print("Waiting for messages on topic: ");
     Serial.println(MQTT_TOPIC);
     Serial.println();
+
+      // The following should be included in (or called from) the main setup routine
+
+    //Get MAC address when joining wifi and place into char array
+    WiFi.macAddress(macAddr);  
+    
+    //Call routing (or embed here) to create initial Unique ID
+    createDiscoveryUniqueID();
+
+    // Define routing
+    server.on("/", handleRoot);
+  
+    //Handle web callbacks for enabling or disabling discovery (using this method is just one of many ways to do this)
+    server.on("/discovery_on",[]() {
+      server.send(200, "text/html", "<h1>Discovery ON...<h1><h3>Home Assistant MQTT Discovery enabled</h3>");
+      delay(200);
+      auto_discovery = true;
+      haDiscovery();
+      Serial.print("Discovery_on");
+    });
+    server.on("/discovery_off",[]() {
+      server.send(200, "text/html", "<h1>Discovery OFF...<h1><h3>Home Assistant MQTT Discovery disabled. Previous entities removed.</h3>");
+      delay(200);
+      auto_discovery = false;
+      haDiscovery();
+      Serial.print("Discovery_off");
+    });
+    server.begin();
+    Serial.print("WebServer Started");
 }
 
 unsigned int countSetBits(unsigned int n)
@@ -237,10 +285,11 @@ void sendAlarm(unsigned int index, char *topic, unsigned int timeout)
     //publish to mqtt
     //publish(topic, "1");
     Serial.println(topic);
-    mqttClient.beginMessage("AudioSensor");
-    mqttClient.print(topic);
-    mqttClient.print("1");
+    mqttClient.beginMessage(topic);
+    //mqttClient.print(topic);
+    mqttClient.print("ON");
     mqttClient.endMessage();
+    Serial.println("MQTT message sent");
 }
 
 void sendMetrics(char * topic, unsigned int mn, unsigned int mx, unsigned int avg)
@@ -346,24 +395,24 @@ void loop(void)
     //Serial.println(peak);
     
     // detecting frequencies around 2550Hz, 1628Hz (Washine Machine)
-    if (detectFrequency(&washingMachine, 7, peak, 116, 74, true))
+    if (detectFrequency(&washingMachine, 7, peak, 116, 175, true))
     {
         Serial.println("Detected Washing Machine");
-        sendAlarm(0, "home/alarm/washingmachine", 2000);
+        sendAlarm(0, "home/alarm/washingmachine", 10000);
     }
 
     //detecting frequencies around 3775Hz, 6767Hz (Smoke Alarm)
-    if (detectFrequency(&fireAlarm, 5, peak, 154, 308, true))
+    if (detectFrequency(&fireAlarm, 5, peak, 153, 164, true))
     {
         Serial.println("Detected fire alarm");
         sendAlarm(1, "home/alarm/fire", 10000);
     }
 
         //detecting frequencies around 2kHz, 3kHz (Dishwasher)
-    if (detectFrequency(&dishWasher, 5, peak, 227, 136, true))
+    if (detectFrequency(&dishWasher, 5, peak, 229, 91, true))
     {
         Serial.println("Detected Dishwasher");
-        sendAlarm(2, "home/alarm/dishwasher", 2000);
+        sendAlarm(2, "home/alarm/dishwasher", 10000);
     }
 
     calculateMetrics(loudness);
@@ -371,6 +420,9 @@ void loop(void)
     // call poll() regularly to allow the library to receive MQTT messages and
     // send MQTT keep alives which avoids being disconnected by the broker
     mqttClient.poll();
+
+      // Handle client requests
+    server.handleClient();
 
    // if(g_loop % 10 == 0) {
    //    mqttClient.beginMessage("AudioSensor");
@@ -381,4 +433,230 @@ void loop(void)
    // }
    // delay(100);
    // g_loop++;
+}
+
+
+
+
+// =====================================
+//  Create Unique ID for topics/entities
+// =====================================
+void createDiscoveryUniqueID() {
+  //Generate UniqueID from uidPrefix + last 6 chars of device MAC address
+  //This should insure that even multiple devices installed in same HA instance are unique
+  
+  strcpy(devUniqueID, uidPrefix);
+  int preSizeBytes = sizeof(uidPrefix);
+  int preSizeElements = (sizeof(uidPrefix) / sizeof(uidPrefix[0]));
+  //Now add last 6 chars from MAC address (these are 'reversed' in the array)
+  int j = 0;
+  for (int i = 2; i >= 0; i--) {
+    sprintf(&devUniqueID[(preSizeBytes - 1) + (j)], "%02X", macAddr[i]);   //preSizeBytes indicates num of bytes in prefix - null terminator, plus 2 (j) bytes for each hex segment of MAC
+    j = j + 2;
+  }
+  // End result is a unique ID for this device (e.g. rctdevE350CA) 
+  Serial.print("Unique ID: ");
+  Serial.println(devUniqueID);  
+}
+// ===============================
+//  Main HA MQTT Discover Function
+//    This creates a single fictional device with four entities:
+//      - A dimmer switch with light level, temperature and IP address
+// ===============================
+void haDiscovery() {
+  char topic[128];
+  if (auto_discovery) {
+    char buffer1[1024];
+    char buffer2[1024];
+    char buffer3[1024];
+    char buffer4[1024];
+    char uid[128];
+    DynamicJsonDocument doc(1024);
+    doc.clear();
+    Serial.println("Discovering new devices...");
+    Serial.println("Adding Audio Alarm Sensors...");
+
+    //Create unique topic based on devUniqueID
+    strcpy(topic, "homeassistant/switch/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "S/config");
+
+    //Create unique_id based on devUniqueID
+    strcpy(uid, devUniqueID);
+    strcat(uid, "S");
+
+    //Create JSON payload per HA documentation
+    doc["name"] = "Audio Alarm Switch";
+    doc["obj_id"] = "mqtt_switch";
+    doc["uniq_id"] = uid;
+    doc["stat_t"] = "homeassistant/mydevice/state";
+    doc["cmd_t"] = "homeassistant/mydevice/set";
+//    doc["brightness"] = "true";
+//    doc["bri_scl"] = "100";
+//    doc["bri_stat_t"] = "stat/mydevice/brightness";
+//    doc["bri_cmd_t"] = "cmnd/mydevice/brightness";
+    JsonObject device = doc.createNestedObject("device");
+    device["ids"] = "mymqttdevice01";
+    device["name"] = "Audio Alarm Sensor";
+    device["mf"] = "Xymed";
+    device["mdl"] = "ESP32";
+    //device["sw"] = "1.00";
+    //device["hw"] = "1.00";
+    //device["cu"] = "http://192.168.1.187/config";  //web interface for device, with discovery toggle
+    serializeJson(doc, buffer1);
+    //size_t n = serializeJson(doc, buffer1, sizeof(buffer1));
+    //Publish discovery topic and payload (with retained flag)
+    //client.publish(topic, buffer1, true); 
+    //if (n > sizeof(buffer1)) {
+    //  Serial.println("Warning: Buffer size is too small for the JSON payload.");
+    //}
+
+    // Print the serialized JSON for debugging
+    //Serial.println("Serialized JSON:");
+    //Serial.println(n);
+    
+    Serial.println(topic);
+    Serial.println(buffer1);
+    Serial.println(true);
+    mqttClient.beginMessage(topic, true);
+    mqttClient.print(buffer1);
+    mqttClient.endMessage();
+
+    //Washing Machine Sensor
+    Serial.println("Adding Washing Machine sensor...");
+    //Create unique Topic based on devUniqueID
+    strcpy(topic, "homeassistant/binary_sensor/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "L/config");
+    //Create unique_id based on decUniqueID
+    strcpy(uid, devUniqueID);
+    strcat(uid, "L");
+    //Create JSON payload per HA documentation
+    doc.clear();
+    doc["name"] = "Washing Machine Tone Detected";
+    doc["obj_id"] = "mqtt_wm_sound";
+    doc["dev_cla"] = "sound";
+    doc["uniq_id"] = uid;
+    doc["stat_t"] = "home/alarm/washingmachine";
+	  doc["pl_on"] = "ON";
+	  doc["pl_off"] = "OFF";
+    JsonObject deviceS = doc.createNestedObject("device");
+    deviceS["ids"] = "mymqttdevice01";
+    deviceS["name"] = "Audio Alarm Sensor";
+    serializeJson(doc, buffer2);
+    //Publish discovery topic and payload (with retained flag)
+    Serial.println(topic), buffer2;  
+    mqttClient.beginMessage(topic, true);
+    mqttClient.print(buffer2);
+    mqttClient.endMessage();  
+    
+    //Temperature Sensor
+    Serial.println("Adding Temp Sensor...");
+    //Create unique Topic based on devUniqueID
+    strcpy(topic, "homeassistant/sensor/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "T/config");
+    //Create unique_id based on decUniqueID
+    strcpy(uid, devUniqueID);
+    strcat(uid, "T");
+    //Create JSON payload per HA documentation
+    doc.clear();
+    doc["name"] = "Temperature";
+    doc["obj_id"] = "mqtt_temperature";
+    doc["dev_cla"] = "temperature";
+    doc["uniq_id"] = uid;
+    doc["stat_t"] = "stat/mydevice/temperature";
+    doc["unit_of_meas"] = "°F";
+    JsonObject deviceT = doc.createNestedObject("device");
+    deviceT["ids"] = "mymqttdevice01";
+    deviceT["name"] = "Audio Alarm Sensor";
+    serializeJson(doc, buffer3);
+    //Publish discovery topic and payload (with retained flag)
+    //client.publish(topic, buffer3, true);
+    mqttClient.beginMessage(topic, true);
+    //mqttClient.print(topic);
+    mqttClient.print(buffer3);
+    //mqttClient.print(true);
+    mqttClient.endMessage();
+    
+    
+    
+    //IP Address Diagnostic
+    Serial.println("Adding IP Diagnostic Sensor...");
+    //Create unique Topic based on devUniqueID
+    strcpy(topic, "homeassistant/sensor/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "I/config");
+    //Create unique_id based on decUniqueID
+    strcpy(uid, devUniqueID);
+    strcat(uid, "I");
+    //Create JSON payload per HA documentation
+    doc.clear();
+    doc["name"] = "IP Address";
+    doc["uniq_id"] = uid;
+    doc["ent_cat"] = "diagnostic";
+    doc["stat_t"] = "stat/mydevice/ipaddress";
+    JsonObject deviceI = doc.createNestedObject("device");
+    deviceI = doc.createNestedObject("device");
+    deviceI["ids"] = "mymqttdevice01";
+    deviceI["name"] = "Audio Alarm Sensor";
+    serializeJson(doc, buffer4);
+    //Publish discovery topic and payload (with retained flag)
+    //client.publish(topic, buffer4, true);
+    mqttClient.beginMessage(topic, true);
+    //mqttClient.print(topic);
+    mqttClient.print(buffer4);
+    //mqttClient.print(true);
+    mqttClient.endMessage();
+    Serial.println("All devices added!");
+
+  } else {
+
+    //Remove all entities by publishing empty payloads
+    //Must use original topic, so recreate from original Unique ID
+    //This will immediately remove/delete the device/entities from HA
+    Serial.println("Removing discovered devices...");
+
+    //Lux Sensor
+    strcpy(topic, "homeassistant/binary_sensor/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "L/config");
+    //client.publish(topic, "");
+    mqttClient.beginMessage(topic);
+    //mqttClient.print(topic);
+    mqttClient.print("");
+    mqttClient.endMessage();
+
+    //Temperature Sensor
+    strcpy(topic, "homeassistant/sensor/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "T/config");
+    //Client.publish(topic, "");
+    mqttClient.beginMessage(topic);
+    //mqttClient.print(topic);
+    mqttClient.print("");
+    mqttClient.endMessage();
+
+    //IP Address Sensor
+    strcpy(topic, "homeassistant/sensor/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "I/config");
+    //Client.publish(topic, "");
+    mqttClient.beginMessage(topic);
+    //mqttClient.print(topic);
+    mqttClient.print("");
+    mqttClient.endMessage();
+
+    //Light (switch)
+    strcpy(topic, "homeassistant/switch/");
+    strcat(topic, devUniqueID);
+    strcat(topic, "S/config");
+    //client.publish(topic, "");
+    mqttClient.beginMessage(topic);
+    //mqttClient.print(topic);
+    mqttClient.print("");
+    mqttClient.endMessage();
+
+    Serial.println("Devices Removed...");
+  }
 }
